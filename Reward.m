44f218b8a11516f8a62e35ff8a6e6a9d989a625f
yang920428 <yangchengwei920428@gmail.com>
2025-04-30 13:43:32 +0800

    finish reward function

--- Reward.m
+++ Reward.m
@@ -1,12 +1,26 @@
 function [R,Terminal]= Reward(robot,a,goal,obs)
 
-%% WRITE YOUR CODE HERE
-R=-0.05;
-Terminal=0;
+    %% WRITE YOUR CODE HERE
+    R=-0.05;
+    Terminal=0;
 
-if (robot.x<=5) || (robot.x>=295) || (robot.y<=5) || (robot.y>=295)
-Terminal=1;
-R=-1*10;    
+    % boundary
+    if (robot.x<=5) || (robot.x>=295) || (robot.y<=5) || (robot.y>=295)
+        Terminal=1;
+        R=-1*10;   
+    % obstacle 
+    elseif (D(robot, obs) < 10)
+        Terminal = 1;
+        R = -10;
+    % goal
+    elseif (D(robot, goal) < 10)
+        Terminal = 1;
+        R = 10;
+    end
+end
+
+function d = D(robot, goal)
+    d = sqrt((robot.x - goal.x)^2 + (robot.y - goal.y));
 end
 
 
