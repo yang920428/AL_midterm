79a17c6b19b3eb15ed5f408d8474cdd19385cbb1
yang920428 <yangchengwei920428@gmail.com>
2025-05-03 16:25:52 +0800

    got a shit

--- simulation/Q_learning.m
+++ simulation/Q_learning.m
@@ -1,12 +1,20 @@
-function [opt_a,Wt,J]= Q_learning(a,W,robot,goal,laser,R,terminal)
-
+function [opt_a,Wt,J]= Q_learning(a,W,robot,goal,laser,R,terminal,prev_robot,prev_laser,prev_a,next_robot,next_laser)
     %% WRITE YOUR CODE HERE
-    alpha = 1e-4;
+    alpha = 1e-3;                  %### 提高學習率，讓權重更新更有效
     gamma = 0.9;
     epsilon = 0.3;
     num_actions = 5;
 
-    f_current = features(robot, goal, laser, a);
+    has_prev = nargin >= 10 && ~isempty(prev_robot) && ~isempty(prev_laser) && ~isempty(prev_a);  %### 更明確的條件檢查
+
+    if has_prev
+        f_prev = features(prev_robot, goal, prev_laser, prev_a);
+        Q_prev = W' * f_prev;
+    else 
+        Q_prev = 0;
+    end
+
+    f_current = features(robot, goal, laser, a);  % 目前狀態 + 現在動作
     Q_current = W' * f_current;
 
     if terminal
@@ -14,25 +22,30 @@ function [opt_a,Wt,J]= Q_learning(a,W,robot,goal,laser,R,terminal)
     else
         Q_next = zeros(num_actions, 1);
         for a_next = 1:num_actions
-            f_next = features(robot, goal, laser, a_next);
+            f_next = features(next_robot, goal, next_laser, a_next);  %### 用 next_state 特徵
             Q_next(a_next) = W' * f_next;
         end
         Q_target = R + gamma * max(Q_next);
     end
 
-    TD_error = Q_target - Q_current;
+    TD_error = Q_target - Q_prev;
     J = TD_error^2;
 
-    Wt = W + alpha * TD_error * f_current;
+    if has_prev  %### 只有在有前一步資訊時才更新 W
+        Wt = W + alpha * TD_error * f_prev;
+    else
+        Wt = W;
+    end
 
+    % Epsilon-greedy action selection based on updated weights
     if rand < epsilon
-        opt_a = randi(num_actions);
+        opt_a = randi(num_actions);  % 探索
     else
         Q_eval = zeros(num_actions,1);
         for a_candidate = 1:num_actions
-            f = features(robot, goal, laser, a_candidate);
+            f = features(robot, goal, laser, a_candidate);  %### 保持使用 current state 選動作
             Q_eval(a_candidate) = Wt' * f;
         end
-    [~, opt_a] = max(Q_eval);
+        [~, opt_a] = max(Q_eval);  % 利用
     end
 end
--- simulation/main.m
+++ simulation/main.m
@@ -1,32 +1,60 @@
 clear all; clc; close all;
 
-Episode=20;
+Episode = 20;
 
-goal.x=150; goal.y=250; % goal location
-obs.x=150; obs.y=150; % obstacle location
-robot0.x=150; robot0.y=50; robot0.t=1.57; % % robot initial location
-W=ones(4,1); % Q-learning weighting
-dt=0.1; % delta t
-CoverMODE=2; M=1;m=1; Eff_robot=1; % GetLaser parameters
+goal.x = 150; goal.y = 250; % goal location
+obs.x = 150; obs.y = 150;   % obstacle location
+robot0.x = 150; robot0.y = 50; robot0.t = 1.57; % robot initial location
+W = ones(4,1); % Q-learning weighting
+dt = 0.1; % delta t
+CoverMODE = 2; M = 1; m = 1; Eff_robot = 1; % GetLaser parameters
 
-index=1;
-for Epi=1:Episode
-    Terminal=0;
-    robot_t_1.x=robot0.x;
-    robot_t_1.y=robot0.y;
-    robot_t_1.t=robot0.t;
-    a=1;
-    while (Terminal==0)
-        [robot_t]= motion_model(robot_t_1,a,dt);
-        [R,Terminal]=Reward(robot_t,a,goal,obs);
-        robot_data(m)=robot_t.x;robot_data(m+M)=robot_t.y;robot_data(m+2*M)=robot_t.t;
-        laser=GetLaser(robot_data(1,1:M),robot_data(1,M+1:2*M),robot_data(1,2*M+1:3*M),Eff_robot,1,CoverMODE); % get laser data         
-        
+index = 1;
+for Epi = 1:Episode
+    Terminal = 0;
+    robot_t_1 = robot0;  % 設定起始點
+    a = 1;
+
+    %%% 新增: 初始化前一狀態的激光數據 %%%
+    prev_laser = zeros(size(GetLaser(robot0.x,robot0.y,robot0.t,Eff_robot,1,CoverMODE)));
+
+    first_step = true; %%% 新增: 標記是否是第一步 %%%
+
+    while (Terminal == 0)
+        [robot_t] = motion_model(robot_t_1, a, dt);  % 執行動作a得到新位置
+        [R, Terminal] = Reward(robot_t, a, goal, obs);
+
+        robot_data(m) = robot_t.x;
+        robot_data(m+M) = robot_t.y;
+        robot_data(m+2*M) = robot_t.t;
+
+        laser = GetLaser(robot_data(1,1:M),robot_data(1,M+1:2*M),robot_data(1,2*M+1:3*M),Eff_robot,1,CoverMODE); % get laser data         
+
+        %### 預測下一步狀態（給 Q-learning 用來計算 TD target）
+        a_tmp = a;  %### 保持目前的動作
+        next_robot = motion_model(robot_t, a_tmp, dt);  %### 模擬下一個 state
+        next_laser = GetLaser(next_robot.x, next_robot.y, next_robot.t, Eff_robot, 1, CoverMODE);  %### 取得下一個雷射感測資訊
+
         pause(0.001);
-        
-        robot_t_1.x=robot_t.x; robot_t_1.y=robot_t.y; robot_t_1.t=robot_t.t;    
-        [a,Wt,J]= Q_learning(a,W,robot_t,goal,laser,R,Terminal);
-        W=Wt;
-        title(['Episode=',num2str(Epi)]);hold off;
-    end   
+
+        %%% 修改: 調用Q_learning時傳遞前一狀態 %%%
+        if first_step
+            % 第一步時沒有真正的"前一狀態"，所以不傳遞
+            [a, Wt, J] = Q_learning(a, W, robot_t, goal, laser, R, Terminal);  %### 無需傳 prev
+            first_step = false;
+        else
+            % 正常情況下傳遞前一狀態與下個狀態
+            [a, Wt, J] = Q_learning(a, W, robot_t, goal, laser, R, Terminal, ...
+                                    robot_t_1, prev_laser, a_prev, ...
+                                    next_robot, next_laser);  %### 傳入 prev + next 狀態資訊
+        end
+
+        %%% 新增: 保存當前狀態和動作作為下一時間步的"前一狀態" %%%
+        a_prev = a;         % 保存當前動作
+        prev_laser = laser; % 保存當前激光數據
+        robot_t_1 = robot_t; % 保存當前位置作為下次的前一位置
+
+        W = Wt;
+        title(['Episode=', num2str(Epi)]); drawnow; hold off;
+    end
 end
