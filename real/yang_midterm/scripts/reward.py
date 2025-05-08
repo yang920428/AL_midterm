e871885ccf4f463eae89a462e6034e791a2202a2
yang920428 <yangchengwei920428@gmail.com>
2025-05-08 13:58:16 +0800

    replacing obs by laser

--- real/yang_midterm/scripts/reward.py
+++ real/yang_midterm/scripts/reward.py
@@ -13,10 +13,10 @@ from laser import laser
 #     goal_distance = ((robot.x-goal.x)**2 + (robot.x-goal.x)**2)**(1/2)
 #     return laser_length/10 + goal_distance/292*10, 0
 
-def reward(robot, action, obs, goal):
+def reward(robot, action, laser, goal):
     
-    if collision_detect(robot, obs):
-        return -10, 1
+    if laser[2] < 20:
+        return -1 / (laser[2] + 0.1), 1
     if goal.arrived_goal(robot.x, robot.y):
         return 10, 1
     # laser_length = laser(robot, obs)[action]
